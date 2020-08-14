6729d01fd56c4dec7e5732487f1ed441b5385b21
reed123456 <3034903604@qq.com>
2020-08-14 17:54:24 +0800

    add 静态库

---
+++ mylib/main.c
@@ -0,0 +1,9 @@
+#include "add.h"
+#include "sub.h"
+
+#include <stdio.h>
+
+int main( void ) {
+	printf("add(3,2)=%d\n", add(3,2));
+	printf("sub(3,2)=%d\n", sub(3,2));
+}
---
+++ mylib/sub.h
@@ -0,0 +1,6 @@
+#ifndef __SUB_H__
+#define __SUB_H__
+
+int sud(int a, int b);
+
+#endif //__SUB_H__
